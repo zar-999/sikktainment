a540f5e0d7d85ac6e01527fd0fc5280cd699bd9b
Peter <hammer.peter@student.htlwrn.ac.at>
2020-02-03 15:44:55 +0100

    delete all

--- php/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbServer = "localhost";
-$dbUsername = "at29620_sikktainmentdb";
-$dbPasswort = "Soldout123!";
-$dbName = "at29620_promoter";
-
-$conn =
-msqli_connect($dbServer,$dbUsername,$dbPasswort,$dbName);
